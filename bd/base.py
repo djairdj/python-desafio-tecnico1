73e6d388c2624d075267b4110d6dbe4888e30fe4
Djair Alves <49504192+djairdj@users.noreply.github.com>
2023-02-06 22:12:30 -0300

    refatorado

--- bd/base.py
+++ bd/base.py
@@ -5,8 +5,6 @@ def cursor():
     database = 'mybase'
     username = os.environ['user_bd'] # aqui substitua pelo seu usuário do banco de dados
     password = os.environ['password_bd'] # aqui substitua pela sua senha do banco de dados
-    #username = 'myuser'
-    #password = 'senha'
     server = 'localhost\\SQLEXPRESS;DATABASE='+database
     url_conn = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';UID=' + username + ';PWD=' + password
 
